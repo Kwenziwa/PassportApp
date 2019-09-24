365b17427d4e90cebf79a63a01aac47f447f9e91
LizwiKagiso <35098232+LizwiKagiso@users.noreply.github.com>
2019-09-24 18:23:16 +0200

    Second push

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,8 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-
-  <title>AdminLTE 3 | Starter</title>
+  <title>{{env('APP_NAME')}}</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <link rel="stylesheet" href="/css/app.css" >
